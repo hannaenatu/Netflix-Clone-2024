12b111788565b8534874bc02ba86be8c3ffa60fc
hannaenatu <hannaenatu@yahoo.com>
2024-09-08 19:48:24 -0400

    Banner

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
+import Banner from '../../Components/Banner/Banner';
+
+
+
 
 
 const Home = () => {
   return (
     <>
    <Header />
+   <Banner />
    <Footer />
     </>
-  )
+  );
 }
 
 export default Home
